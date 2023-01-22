67f2c3236eebc64b61acb329b13972d4eadc2948
patthapeegeo <120860330+patthapeegeo@users.noreply.github.com>
2023-01-22 09:46:50 +0700

    Add function to trigger countdown

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const alertButton = document.querySelector("button");
+const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime()
 
-alertButton.addEventListener("click", function () {
-  alert("Hello! this is button from second commit!!!");
-});
+// document.getElementById("countdown").innerHTML = countDownDate;
+
+const  countDownTrigger = () => {
+    const now = new Date().getTime();
+
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    document.getElementById("countdown").innerHTML = days;
+};
+
+countDownTrigger();
